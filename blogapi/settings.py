ac970913fdfb143cb7b70afc0d8ae67545f81cc1
sohaib <suhaibaledlah@gmail.com>
2025-05-09 08:59:16 +0300

    allow all hosts

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7jht4ycfg3f)n+6y1%+7b_nbd$#r-byt7sj&buk%$c)@@pl4f2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
